65ae83d353716b28cbe0daec63d6e04a420b415c
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-30 23:41:47 +0900

    3

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,33 @@
+/** @format */
+
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-east-1' });
+
 async function sendMail(event, context) {
-	console.log(event);
-	return event;
+	const params = {
+		Source: 'kazuki0924@gmail.com',
+		Destination: {
+			ToAddresses: ['kazuki0924@gmail.com'],
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: 'Hello from AWS',
+				},
+			},
+			Subject: {
+				Data: 'Test Mail',
+			},
+		},
+	};
+	try {
+		const result = await ses.sendEmail(params).promise();
+		console.log(result);
+		return result;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 export const handler = sendMail;
